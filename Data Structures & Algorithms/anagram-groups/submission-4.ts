5667645d5e990c2149ba1031a6685c3c6ec250fe
arthuracrs <30377743+arthuracrs@users.noreply.github.com>
2026-06-23 12:25:22 -0300

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.ts
@@ -0,0 +1,57 @@
+class Solution {
+    groupAnagrams(strs: string[]): string[][] {
+        const mapAlfabeto = {
+            a: 0,
+            b: 1,
+            c: 2,
+            d: 3,
+            e: 4,
+            f: 5,
+            g: 6,
+            h: 7,
+            i: 8,
+            j: 9,
+            k: 10,
+            l: 11,
+            m: 12,
+            n: 13,
+            o: 14,
+            p: 15,
+            q: 16,
+            r: 17,
+            s: 18,
+            t: 19,
+            u: 20,
+            v: 21,
+            w: 22,
+            x: 23,
+            y: 24,
+            z: 25,
+        };
+
+        const osAnagramas = {};
+
+        for (let i = 0; i < strs.length; i++) {
+            const counter = new Array(26).fill(0);
+
+            const arrayDaString = strs[i].split("");
+            for (let j = 0; j < arrayDaString.length; j++) {
+                counter[mapAlfabeto[arrayDaString[j]]] += 1;
+            }
+            const anagramId = counter.join("#")
+            if (osAnagramas[anagramId] === undefined) {
+                osAnagramas[anagramId] = [];
+            }
+
+            osAnagramas[anagramId].push(strs[i])
+        }
+        console.log(osAnagramas)
+        const result = []
+
+        for(const values of Object.values(osAnagramas)) {
+            result.push(values)
+        }
+
+        return result
+    }
+}
